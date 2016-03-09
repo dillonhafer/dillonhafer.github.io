73a6560a438930dbdebf07f569dd3986fce7d8a7
Dillon Hafer <dh@dillonhafer.com>
2016-03-08 18:22:09 -0600

    Remove jquery from projects and update styles

--- js/hc.js
+++ js/hc.js
@@ -1,5 +1,23 @@
-$(function() {
-    $('.project-box').click(function() {
-        $(this).find('.post').slideToggle();
-    });
-});
+(function() {
+  var toggle = function(el, className) {
+    if (el.classList) {
+      el.classList.toggle(className);
+    } else {
+      var classes = el.className.split(' ');
+      var existingIndex = classes.indexOf(className);
+
+      if (existingIndex >= 0)
+        classes.splice(existingIndex, 1);
+      else
+        classes.push(className);
+
+      el.className = classes.join(' ');
+    }
+  }
+
+  var boxes = document.querySelectorAll('.project-box');
+  for (i = 0; i < boxes.length; i++) {
+    var box = boxes[i];
+    box.addEventListener('click', function() { toggle(box, 'toggled'); });
+  }
+})()
