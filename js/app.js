1cf66d8abc5ab9bc8c770e760e6c07556b23d39c
Dillon Hafer <dh@dillonhafer.com>
2016-03-10 22:22:40 -0600

    Housekeeping

--- js/app.js
+++ js/app.js
@@ -2,10 +2,12 @@
   var menuButton = document.querySelector('.navbar-toggle');
   menuButton.addEventListener('click', function() {
     var nav = document.querySelector('.navbar-collapse');
+    var classes = 'navbar-collapse collapse';
+
     if (nav.getAttribute('class') === 'navbar-collapse collapse') {
-      nav.setAttribute('class', 'navbar-collapse');
-    } else {
-      nav.setAttribute('class', 'navbar-collapse collapse');
+      classes = 'navbar-collapse';
     }
+
+    nav.setAttribute('class', classes);
   });
 })()
